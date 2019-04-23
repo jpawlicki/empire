83871fc4de1bba981851cfee6f06fa37ffce8f3e
Alexander Pawlicki <alex__pawlicki@bose.com>
2019-04-23 09:47:00 -0400

    Pulled hardcoded values out of Character

--- backend/src/main/java/com/empire/Character.java
+++ backend/src/main/java/com/empire/Character.java
@@ -1,11 +1,42 @@
 package com.empire;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 final class Character {
+	private static final double basePlotStrength = 1.0;
+	private static final double guardAgainstPlotMod = 0.5;
+	private static final double perSpyLevelPlotMod = 0.3;
+	private static final double lyskrPlotMod = 0.4;
+	private static final double companyPlotMod = 0.2;
+	private static final double perInspirePlotMod = 0.05;
+	private static final double capturedPlotMod = -0.5;
+
+	private static final String charDimAll = "*";
+	private static final String charDimGeneral = "general";
+	private static final String charDimAdmiral = "admiral";
+	private static final String charDimSpy = "spy";
+	private static final String charDimGovernor = "governor";
+	private static final String[] charDims = new String[]{charDimGeneral, charDimAdmiral, charDimSpy, charDimGovernor};
+
+	private static final int minDimLevel = 1;
+	private static final double oneDimExpAdd = 1.0;
+	private static final double allDimExpAdd = 0.25;
+
+	private static final String nationHeroicTag = "Heroic";
+	private static final double heroicExpMultiplier = 2.0;
+
+	private static final String noCaptor = "";
+
+	// This map is ordered by insertion order, code below depends on the keys being in descending order
+	// Perhaps change to a TreeMap which orders keys internally (although in ascending order)
+	private static final Map<Double, Integer> expLevels = new LinkedHashMap<>();
+	static {
+		expLevels.put(24.0, 5);
+		expLevels.put(15.0, 4);
+		expLevels.put(8.0, 3);
+		expLevels.put(3.0, 2);
+	}
+
 	String name = "";
 	String kingdom = "";
 	String captor = "";
@@ -21,29 +52,37 @@ final class Character {
 
 	public int calcLevel(String dimension) {
 		double xp = experience.get(dimension);
-		if (xp >= 24) return 5;
-		else if (xp >= 15) return 4;
-		else if (xp >= 8) return 3;
-		else if (xp >= 3) return 2;
-		else return 1;
+
+		return expLevels.entrySet().stream().
+				filter(e -> xp >= e.getKey()).
+				map(Map.Entry::getValue).
+				findFirst().
+				orElse(minDimLevel);
 	}
 
 	public double calcPlotPower(World w, boolean boosted, int inspires) {
-		double power = 1;
-		if (boosted) power += 0.5;
-		power += calcLevel("spy") * 0.3;
-		if (Ideology.LYSKR == NationData.getStateReligion(kingdom, w)) power += .4;
-		if (Ideology.COMPANY == NationData.getStateReligion(kingdom, w)) power += .2;
-		if (NationData.getStateReligion(kingdom, w).religion == Religion.IRUHAN) power += inspires * .05;
-		if (!"".equals(captor)) power -= 0.5;
+		double power = basePlotStrength;
+
+		power += calcLevel(charDimSpy) * perSpyLevelPlotMod;
+
+		if (boosted) power += guardAgainstPlotMod;
+		if (Ideology.LYSKR == NationData.getStateReligion(kingdom, w)) power += lyskrPlotMod;
+		if (Ideology.COMPANY == NationData.getStateReligion(kingdom, w)) power += companyPlotMod;
+		if (NationData.getStateReligion(kingdom, w).religion == Religion.IRUHAN) power += inspires * perInspirePlotMod;
+		if (!noCaptor.equals(captor)) power -= capturedPlotMod;
+
 		return power;
 	}
 
 	public void addExperience(String dimension, World w) {
-		if ("*".equals(dimension)) {
-			for (String d : new String[]{"general", "admiral", "spy", "governor"}) experience.put(d, experience.get(d) + (w.getNation(kingdom).hasTag("Heroic") ? .5 : .25));
+		if (charDimAll.equals(dimension)) {
+			for (String d : charDims) {
+				double expAdd = w.getNation(kingdom).hasTag(nationHeroicTag) ? heroicExpMultiplier * allDimExpAdd : allDimExpAdd;
+				experience.put(d, experience.get(d) + expAdd);
+			}
 		} else {
-			experience.put(dimension, experience.get(dimension) + (w.getNation(kingdom).hasTag("Heroic") ? 2 : 1));
+			double expAdd = w.getNation(kingdom).hasTag(nationHeroicTag) ? heroicExpMultiplier * oneDimExpAdd : oneDimExpAdd;
+			experience.put(dimension, experience.get(dimension) + expAdd);
 		}
 	}
 
