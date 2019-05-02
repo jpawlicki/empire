efe7eaf855d6f42e8d9fe7c56e26ff10582a0eee
Alexander Pawlicki <alex__pawlicki@bose.com>
2019-05-02 13:38:21 -0400

    Unit test for character levels

---
+++ backend/src/test/java/com/empire/CharacterTest.java
@@ -0,0 +1,43 @@
+package com.empire;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class CharacterTest {
+    Character c;
+
+    @Before
+    public void createCharacter(){
+        c = new Character();
+    }
+
+    private void setExperience(double exp){
+        setExperience(exp, exp, exp, exp);
+    }
+
+    private void setExperience(double general, double admiral, double governor, double spy){
+        c.experience.put(Constants.charDimGeneral, general);
+        c.experience.put(Constants.charDimAdmiral, admiral);
+        c.experience.put(Constants.charDimGovernor, governor);
+        c.experience.put(Constants.charDimSpy, spy);
+    }
+
+    @Test
+    public void calcLevelAll(){
+        List<Double> expLevel = Arrays.asList(1.0, 4.0, 9.0, 16.0, 25.0);
+
+        for(int level = 1; level <= 5; level++) {
+            setExperience(expLevel.get(level-1));
+            assertEquals(level, c.calcLevel(Constants.charDimGeneral));
+            assertEquals(level, c.calcLevel(Constants.charDimAdmiral));
+            assertEquals(level, c.calcLevel(Constants.charDimGovernor));
+            assertEquals(level, c.calcLevel(Constants.charDimSpy));
+        }
+    }
+
+}
