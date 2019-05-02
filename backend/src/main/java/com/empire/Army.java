7b03e79e6686533df7b7464340dfab3e1f26e6f8
Alexander Pawlicki <alex__pawlicki@bose.com>
2019-05-02 10:03:38 -0400

    Unit test for loyal noble

--- backend/src/main/java/com/empire/Army.java
+++ backend/src/main/java/com/empire/Army.java
@@ -36,7 +36,7 @@ final class Army {
 		if (hasTag(Constants.armySeafaringTag) && r.isSea()) mods += Constants.seafaringMod;
 		if (isArmy() && !kingdom.equals(Constants.armyPirateTag) && w.getNation(kingdom).hasTag(Constants.nationDisciplinedTag)) mods += Constants.disciplinedMod;
 		if (isArmy() && r.isLand() && NationData.isFriendly(r.kingdom, kingdom, w)) mods += r.calcFortification() - 1;
-		if (isArmy() && r.noble != null && r.noble.hasTag(Constants.nobleLoyalTag) && r.kingdom.equals(kingdom)) mods += Constants.loyalMod;
+		if (isArmy() && r.noble != Constants.noNoble && r.noble.hasTag(Constants.nobleLoyalTag) && r.kingdom.equals(kingdom)) mods += Constants.loyalMod;
 		if (Ideology.SWORD_OF_TRUTH == w.getDominantIruhanIdeology()) {
 			Ideology sr = NationData.getStateReligion(kingdom, w);
 			if (Ideology.SWORD_OF_TRUTH == sr) mods += Constants.swordOfTruthMod;
@@ -44,7 +44,7 @@ final class Army {
 		}
 		if (lastStand) mods += Constants.lastStandMod;
 		if (isArmy() && NationData.getStateReligion(kingdom, w).religion == Religion.IRUHAN) mods += inspires * Constants.perInspireMod;
-		if (leader != null && leader.captor.equals(Constants.noCaptor)) mods += leader.calcLevel(isArmy() ? Constants.charDimGeneral : Constants.charDimAdmiral) * Constants.perLevelLeaderMod;
+		if (leader != Constants.noLeader && leader.captor.equals(Constants.noCaptor)) mods += leader.calcLevel(isArmy() ? Constants.charDimGeneral : Constants.charDimAdmiral) * Constants.perLevelLeaderMod;
 
 		return strength * mods;
 	}
