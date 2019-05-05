94427bcfadf770c23b1a4cc5d57bd5fe29320700
Alexander Pawlicki <alexanderpawlicki@gmail.com>
2019-05-05 11:27:33 -0400

    final uppercase delta constant for army and character tests, fixed some code style while I was at it

--- backend/src/test/java/com/empire/ArmyTest.java
+++ backend/src/test/java/com/empire/ArmyTest.java
@@ -10,22 +10,22 @@ import java.util.Arrays;
 public class ArmyTest {
 	private static Army plainArmy;
 	private static World world;
-	private static double delta = 1E-5;
+	private static final double DELTA = 1E-5;
 
-	private void addFortification(){
+	private void addFortification() {
 		Construction fort = new Construction();
 		fort.type = Constants.constFort;
 		world.regions.get(0).constructions = Arrays.asList(fort, fort);
 	}
 
-	private Character getLeader(){
+	private Character getLeader() {
 		Character leader = new Character();
 		leader.experience.put(Constants.charDimGeneral, 3.0);
 		leader.experience.put(Constants.charDimAdmiral, 3.0);
 		return leader;
 	}
 
-	private void makeSwordOfTruthDominant(){
+	private void makeSwordOfTruthDominant() {
 		Region r = new Region();
 		r.religion = Ideology.SWORD_OF_TRUTH;
 		r.population = 1E9;
@@ -33,7 +33,7 @@ public class ArmyTest {
 	}
 
 	@Before
-	public void setUpPlainArmy(){
+	public void setUpPlainArmy() {
 		plainArmy = new Army();
 		plainArmy.type = Army.Type.ARMY;
 		plainArmy.size = 100.0;
@@ -44,160 +44,160 @@ public class ArmyTest {
 	}
 
 	@Test
-	public void calcStrengthBasic(){
-		assertEquals(1.0, plainArmy.calcStrength(world, null, 0, false), delta);
+	public void calcStrengthBasic() {
+		assertEquals(1.0, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthNavy(){
+	public void calcStrengthNavy() {
 		plainArmy.type = Army.Type.NAVY;
-		assertEquals(100.0, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(100.0, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthSteel(){
+	public void calcStrengthSteel() {
 		plainArmy.addTag(Constants.armySteelTag);
-		assertEquals(1.15, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(1.15, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthSeafaringOnLand(){
+	public void calcStrengthSeafaringOnLand() {
 		plainArmy.addTag(Constants.armySeafaringTag);
-		assertEquals(1.0, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(1.0, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthSeafaring(){
+	public void calcStrengthSeafaring() {
 		plainArmy.addTag(Constants.armySeafaringTag);
 		world.regions.get(0).type = Region.Type.WATER;
-		assertEquals(2.5, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(2.5, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthDisciplinedNavy(){
+	public void calcStrengthDisciplinedNavy() {
 		world.getNation("k1").addTag(Constants.nationDisciplinedTag);
 		plainArmy.type = Army.Type.NAVY;
-		assertEquals(100.0, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(100.0, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthDisciplinedPirate(){
+	public void calcStrengthDisciplinedPirate() {
 		world.getNation("k1").addTag(Constants.nationDisciplinedTag);
 		plainArmy.kingdom = Constants.armyPirateTag;
-		assertEquals(1.0, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(1.0, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthDisciplined(){
+	public void calcStrengthDisciplined() {
 		world.getNation("k1").addTag(Constants.nationDisciplinedTag);
-		assertEquals(1.1, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(1.1, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthFortificationNavy(){
+	public void calcStrengthFortificationNavy() {
 		addFortification();
 		plainArmy.type = Army.Type.NAVY;
-		assertEquals(100.0, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(100.0, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthFortificationWater(){
+	public void calcStrengthFortificationWater() {
 		addFortification();
 		world.regions.get(0).type = Region.Type.WATER;
-		assertEquals(1.0, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(1.0, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthFortificationNotFriendly(){
+	public void calcStrengthFortificationNotFriendly() {
 		addFortification();
 		plainArmy.kingdom = "k2";
-		assertEquals(1.0, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(1.0, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthFortification(){
+	public void calcStrengthFortification() {
 		addFortification();
-		assertEquals(1.3, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(1.3, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthLoyalNavy(){
+	public void calcStrengthLoyalNavy() {
 		plainArmy.location = 1;
 		plainArmy.type = Army.Type.NAVY;
-		assertEquals(100.0, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(100.0, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthLoyalNotRegionOwner(){
+	public void calcStrengthLoyalNotRegionOwner() {
 		plainArmy.location = 1;
 		world.regions.get(1).kingdom = "k2";
-		assertEquals(1.0, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(1.0, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthLoyal(){
+	public void calcStrengthLoyal() {
 		plainArmy.location = 1;
-		assertEquals(1.25, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(1.25, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthSwordOfTruthNonIruhan(){
+	public void calcStrengthSwordOfTruthNonIruhan() {
 		makeSwordOfTruthDominant();
 		world.regions.get(0).religion = Ideology.ALYRJA;
 		world.regions.get(0).population = 1.0;
-		assertEquals(1.0, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(1.0, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthSwordOfTruthAligned(){
+	public void calcStrengthSwordOfTruthAligned() {
 		makeSwordOfTruthDominant();
 		world.regions.get(0).religion = Ideology.SWORD_OF_TRUTH;
 		world.regions.get(0).population = 1.0;
-		assertEquals(1.25, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(1.25, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthSwordOfTruthNonAligned(){
+	public void calcStrengthSwordOfTruthNonAligned() {
 		makeSwordOfTruthDominant();
 		world.regions.get(0).religion = Ideology.CHALICE_OF_COMPASSION;
 		world.regions.get(0).population = 1.0;
-		assertEquals(1.15, plainArmy.calcStrength(world, null, 0, false), delta);
+		assertEquals(1.15, plainArmy.calcStrength(world, null, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthLastStand(){
-		assertEquals(5.0, plainArmy.calcStrength(world, null, 0, true), delta);
+	public void calcStrengthLastStand() {
+		assertEquals(5.0, plainArmy.calcStrength(world, null, 0, true), DELTA);
 	}
 
 	@Test
-	public void calcStrengthInspireNavy(){
+	public void calcStrengthInspireNavy() {
 		world.regions.get(0).religion = Ideology.CHALICE_OF_COMPASSION;
 		plainArmy.type = Army.Type.NAVY;
-		assertEquals(100.0, plainArmy.calcStrength(world, null, 2, false), delta);
+		assertEquals(100.0, plainArmy.calcStrength(world, null, 2, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthInspire(){
+	public void calcStrengthInspire() {
 		world.regions.get(0).religion = Ideology.CHALICE_OF_COMPASSION;
 		world.regions.get(0).population = 1.0;
-		assertEquals(1.1, plainArmy.calcStrength(world, null, 2, false), delta);
+		assertEquals(1.1, plainArmy.calcStrength(world, null, 2, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthCaptured(){
+	public void calcStrengthCaptured() {
 		Character leader = getLeader();
 		leader.captor = "DONTCARE";
-		assertEquals(1.0, plainArmy.calcStrength(world, leader, 0, false), delta);
+		assertEquals(1.0, plainArmy.calcStrength(world, leader, 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthGeneral(){
-		assertEquals(1.4, plainArmy.calcStrength(world, getLeader(), 0, false), delta);
+	public void calcStrengthGeneral() {
+		assertEquals(1.4, plainArmy.calcStrength(world, getLeader(), 0, false), DELTA);
 	}
 
 	@Test
-	public void calcStrengthAdmiral(){
+	public void calcStrengthAdmiral() {
 		plainArmy.type = Army.Type.NAVY;
-		assertEquals(140.0, plainArmy.calcStrength(world, getLeader(), 0, false), delta);
+		assertEquals(140.0, plainArmy.calcStrength(world, getLeader(), 0, false), DELTA);
 	}
 }
--- backend/src/test/java/com/empire/CharacterTest.java
+++ backend/src/test/java/com/empire/CharacterTest.java
@@ -11,10 +11,10 @@ import static org.junit.Assert.assertEquals;
 public class CharacterTest {
     private Character c;
     private static World world;
-    private static double delta = 1E-5;
+    private static final double DELTA = 1E-5;
 
     @Before
-    public void createCharacter(){
+    public void createCharacter() {
         c = new Character();
         c.kingdom = "k1";
         setExperience(0.0);
@@ -23,30 +23,30 @@ public class CharacterTest {
         world.regions.get(0).religion = Ideology.SWORD_OF_TRUTH;
     }
 
-    private void setExperience(double exp){
+    private void setExperience(double exp) {
         setExperience(exp, exp, exp, exp);
     }
 
-    private void setExperience(double general, double admiral, double governor, double spy){
+    private void setExperience(double general, double admiral, double governor, double spy) {
         c.experience.put(Constants.charDimGeneral, general);
         c.experience.put(Constants.charDimAdmiral, admiral);
         c.experience.put(Constants.charDimGovernor, governor);
         c.experience.put(Constants.charDimSpy, spy);
     }
 
-    private void addHeroicTag(){
+    private void addHeroicTag() {
         world.getNation("k1").addTag(Constants.nationHeroicTag);
     }
 
-    private void assertDimsExpEqual(double general, double admiral, double governor, double spy){
-        assertEquals(general, c.getExperience(Constants.charDimGeneral), delta);
-        assertEquals(admiral, c.getExperience(Constants.charDimAdmiral), delta);
-        assertEquals(governor, c.getExperience(Constants.charDimGovernor), delta);
-        assertEquals(spy, c.getExperience(Constants.charDimSpy), delta);
+    private void assertDimsExpEqual(double general, double admiral, double governor, double spy) {
+        assertEquals(general, c.getExperience(Constants.charDimGeneral), DELTA);
+        assertEquals(admiral, c.getExperience(Constants.charDimAdmiral), DELTA);
+        assertEquals(governor, c.getExperience(Constants.charDimGovernor), DELTA);
+        assertEquals(spy, c.getExperience(Constants.charDimSpy), DELTA);
     }
 
     @Test
-    public void calcLevelAll(){
+    public void calcLevelAll() {
         List<Double> expLevel = Arrays.asList(1.0, 4.0, 9.0, 16.0, 25.0);
 
         for(int level = 1; level <= 5; level++) {
@@ -59,120 +59,120 @@ public class CharacterTest {
     }
 
     @Test(expected=NullPointerException.class)
-    public void calcLevelUnknownDimThrowsError(){
+    public void calcLevelUnknownDimThrowsError() {
         // Not a great test but have something in here about unknown keys, could be fixed by using enum
         c.calcLevel("DUMMY");
     }
 
     @Test
-    public void addExperienceGeneralRegular(){
+    public void addExperienceGeneralRegular() {
         c.addExperience(Constants.charDimGeneral, world);
         assertDimsExpEqual(1.0, 0.0, 0.0, 0.0);
     }
 
     @Test
-    public void addExperienceAdmirallRegular(){
+    public void addExperienceAdmirallRegular() {
         c.addExperience(Constants.charDimAdmiral, world);
         assertDimsExpEqual(0.0, 1.0, 0.0, 0.0);
     }
 
     @Test
-    public void addExperienceGovernorRegular(){
+    public void addExperienceGovernorRegular() {
         c.addExperience(Constants.charDimGovernor, world);
         assertDimsExpEqual(0.0, 0.0, 1.0, 0.0);
     }
 
     @Test
-    public void addExperienceSpyRegular(){
+    public void addExperienceSpyRegular() {
         c.addExperience(Constants.charDimSpy, world);
         assertDimsExpEqual(0.0, 0.0, 0.0, 1.0);
 
     }
 
     @Test
-    public void addExperienceAllRegular(){
+    public void addExperienceAllRegular() {
         c.addExperience(Constants.charDimAll, world);
         assertDimsExpEqual(0.25, 0.25, 0.25, 0.25);
     }
 
     @Test
-    public void addExperienceGeneralHeroic(){
+    public void addExperienceGeneralHeroic() {
         addHeroicTag();
         c.addExperience(Constants.charDimGeneral, world);
         assertDimsExpEqual(2.0, 0.0, 0.0, 0.0);
     }
 
     @Test
-    public void addExperienceAdmiralHeroic(){
+    public void addExperienceAdmiralHeroic() {
         addHeroicTag();
         c.addExperience(Constants.charDimAdmiral, world);
         assertDimsExpEqual(0.0, 2.0, 0.0, 0.0);
     }
 
     @Test
-    public void addExperienceGovernorHeroic(){
+    public void addExperienceGovernorHeroic() {
         addHeroicTag();
         c.addExperience(Constants.charDimGovernor, world);
         assertDimsExpEqual(0.0, 0.0, 2.0, 0.0);
     }
 
     @Test
-    public void addExperienceSpyHeroic(){
+    public void addExperienceSpyHeroic() {
         addHeroicTag();
         c.addExperience(Constants.charDimSpy, world);
         assertDimsExpEqual(0.0, 0.0, 0.0, 2.0);
     }
 
     @Test
-    public void addExperienceAllHeroic(){
+    public void addExperienceAllHeroic() {
         addHeroicTag();
         c.addExperience(Constants.charDimAll, world);
         assertDimsExpEqual(0.5, 0.5, 0.5, 0.5);
     }
 
     @Test
-    public void calcPlotPowerBasic(){
-        assertEquals(1.3, c.calcPlotPower(world, false, 0), delta);
+    public void calcPlotPowerBasic() {
+        assertEquals(1.3, c.calcPlotPower(world, false, 0), DELTA);
     }
 
     @Test
-    public void calcPlotPowerSpyLevel(){
+    public void calcPlotPowerSpyLevel() {
         setExperience(4.0);
-        assertEquals(1.6, c.calcPlotPower(world, false, 0), delta);
+        assertEquals(1.6, c.calcPlotPower(world, false, 0), DELTA);
     }
 
     @Test
-    public void calcPlotPowerGuarding(){
-        assertEquals(1.8, c.calcPlotPower(world, true, 0), delta);
+    public void calcPlotPowerGuarding() {
+        assertEquals(1.8, c.calcPlotPower(world, true, 0), DELTA);
     }
 
     @Test
-    public void calcPlotPowerLyskr(){
+    public void calcPlotPowerLyskr() {
         world.regions.get(0).religion = Ideology.LYSKR;
-        assertEquals(1.7, c.calcPlotPower(world, false, 0), delta);
+        assertEquals(1.7, c.calcPlotPower(world, false, 0), DELTA);
     }
 
     @Test
-    public void calcPlotPowerCompany(){
+    public void calcPlotPowerCompany() {
         world.regions.get(0).religion = Ideology.COMPANY;
-        assertEquals(1.5, c.calcPlotPower(world, false, 0), delta);
+        assertEquals(1.5, c.calcPlotPower(world, false, 0), DELTA);
     }
 
     @Test
-    public void calcPlotPowerInspireNonIruhan(){
+    public void calcPlotPowerInspireNonIruhan() {
         world.regions.get(0).religion = Ideology.ALYRJA;
-        assertEquals(1.3, c.calcPlotPower(world, false, 2), delta);
+        assertEquals(1.3, c.calcPlotPower(world, false, 2), DELTA);
     }
 
     @Test
-    public void calcPlotPowerInspireIruhan(){
+    public void calcPlotPowerInspireIruhan() {
         world.regions.get(0).religion = Ideology.SWORD_OF_TRUTH;
-        assertEquals(1.4, c.calcPlotPower(world, false, 2), delta);
+        assertEquals(1.4, c.calcPlotPower(world, false, 2), DELTA);
     }
 
     @Test
-    public void calcPlotPowerCaptive(){
+    public void calcPlotPowerCaptive() {
         c.captor = "DONTCARE";
-        assertEquals(0.8, c.calcPlotPower(world, false, 0), delta);
+        assertEquals(0.8, c.calcPlotPower(world, false, 0), DELTA);
     }
 }
