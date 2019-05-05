f8d9b292640529cf422758e5a4c3c630a22f758b
Alexander Pawlicki <alexanderpawlicki@gmail.com>
2019-05-05 11:34:31 -0400

    Fixed * import

--- backend/src/main/java/com/empire/Character.java
+++ backend/src/main/java/com/empire/Character.java
@@ -1,6 +1,10 @@
 package com.empire;
 
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
 
 final class Character {
 	String name = "";
