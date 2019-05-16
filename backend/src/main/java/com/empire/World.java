c55a99cd41076fce9d9c7de80ebc43454838ac05
Joshua Pawlicki <gilgarn@gmail.com>
2019-05-16 06:13:01 -0700

    Fortifications reduce friendly casualties.

--- backend/src/main/java/com/empire/World.java
+++ backend/src/main/java/com/empire/World.java
@@ -1409,6 +1409,9 @@ final class World {
 					if (enemyStrength == 0) continue;
 					double cf = enemyStrength / totalArmyStrength;
 					if (a.hasTag("Formations") && cf > .45 && cf < .9) cf = .45;
+					if (region.kingdom != null && NationData.isFriendly(region.kingdom, a.kingdom, World.this, region)) {
+						cf *= Math.max(0, Math.min(1, 1 - (region.calcFortification() - 1)));
+					}
 					if (cf >= .9) cf = 1;
 					if (cf != 0) casualties.put(a, cf);
 					for (Army b : localArmies) {
