ed466ea1fcd412fe764ae033a6812ad333156f12
Joshua Pawlicki <gilgarn@gmail.com>
2019-05-16 03:44:05 -0700

    Move raze evaluation into army. Fix bugs with religion assignment.

--- backend/src/main/java/com/empire/StringUtil.java
+++ backend/src/main/java/com/empire/StringUtil.java
@@ -7,10 +7,15 @@ public class StringUtil {
 		StringBuilder b = new StringBuilder();
 		for (int i = 0; i < in.size(); i++) {
 			if (i > 0 && in.size() > 2) b.append(", ");
-			if (i > 0 && i == in.size() - 1) b.append("and ");
+			if (i > 0 && i == in.size() - 1) b.append(" and ");
 			b.append(in.get(i));
 		}
 		return b.toString();
+	}
+
+	public static String quantify(int num, String unit) {
+		if (unit.endsWith("s")) return num + " " + unit;
+		return num + " " + unit + num != 1 ? "s" : "";
 	}
 
 	private StringUtil() {}
