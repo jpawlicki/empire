5cd4a01238291c53764ce69c6bbe0b164ddfcd1d
Alexander Pawlicki <alex__pawlicki@bose.com>
2019-05-22 13:19:29 -0400

    Started using a mocked world for Region tests

--- backend/src/test/java/com/empire/RegionTest.java
+++ backend/src/test/java/com/empire/RegionTest.java
@@ -1,27 +1,53 @@
 package com.empire;
 
+import java.util.Arrays;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class RegionTest {
     private static Region r;
     private static World world;
     private static final double DELTA = 1E-5;
 
+    private static final String k1 = "k1";
+    private static final String k2 = "k2";
+    private static final String k3 = "k3";
+
     @Before
     public void setUpRregion() {
         r = new Region();
 
-        world = WorldTest.regionTestWorld();
+//        world = WorldTest.regionTestWorld();
+        world = mockWorld();
+    }
+
+    private World mockWorld(){
+        World mockWorld = mock(World.class);
+        Region r0 = mockRegion(k1, Region.Type.LAND, Ideology.CHALICE_OF_COMPASSION);
+        Region r1 = mockRegion(k1, Region.Type.LAND, Ideology.CHALICE_OF_COMPASSION);
+        Region r2 = mockRegion(k1, Region.Type.LAND, Ideology.CHALICE_OF_COMPASSION);
+        Region r3 = mockRegion(k2, Region.Type.LAND, null);
+        Region r4 = mockRegion(k3, Region.Type.WATER, null);
+        mockWorld.regions = Arrays.asList(r0, r1, r2, r3, r4);
+        return mockWorld;
+    }
+
+    private Region mockRegion(String kingdom, Region.Type type, Ideology religion){
+        Region r = mock(Region.class);
+        when(r.getKingdom()).thenReturn(kingdom);
+        r.type = type;
+        r.religion = religion;
+        return r;
     }
 
     @Test
     public void numUniqueIdeologiesTest(){
-        assertEquals(2, Region.numUniqueIdeologies("k1", world));
-        assertEquals(2, Region.numUniqueIdeologies2("k1", world));
+        assertEquals(2, Region.numUniqueIdeologies(k1, world));
+        assertEquals(2, Region.numUniqueIdeologies2(k1, world));
     }
 
     // TODO:  The can transfer food tests rely on making a test world that is too complex at the moment, these should
