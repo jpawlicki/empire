a2b70a4e0dac167c6a87ca40f831bb49886668d0
Alexander Pawlicki <alexanderpawlicki@gmail.com>
2019-05-23 17:33:17 -0400

    Mock for character and fixed region population causing tests to fail

--- backend/src/test/java/com/empire/Mocks.java
+++ backend/src/test/java/com/empire/Mocks.java
@@ -4,7 +4,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class Mocks {
-    public static Region region(String kingdom, Region.Type type, double population, Ideology religion){
+    public static Region region(String kingdom, Region.Type type, double population, Ideology religion) {
         Region r = mock(Region.class);
         when(r.getKingdom()).thenReturn(kingdom);
         r.type = type;
@@ -19,5 +19,19 @@ class Mocks {
         n.name = "DONTCARE";
         when(n.hasTag(nobleTag)).thenReturn(true);
         return n;
+    }
+
+    public static Character character(double general, double admiral, double governor, double spy) {
+        Character c = mock(Character.class);
+        c.captor = Constants.noCaptor;
+        when(c.getExperience(Constants.charDimGeneral)).thenReturn(general);
+        when(c.getExperience(Constants.charDimAdmiral)).thenReturn(admiral);
+        when(c.getExperience(Constants.charDimGovernor)).thenReturn(governor);
+        when(c.getExperience(Constants.charDimSpy)).thenReturn(spy);
+        return c;
+    }
+
+    public static Character character(double xp) {
+        return Mocks.character(xp, xp, xp, xp);
     }
 }
