ca8ff5c224d2614d80e58733f229c8fd2624237f
Alexander Pawlicki <alex__pawlicki@bose.com>
2019-05-23 10:04:41 -0400

    Ordered imports in test package

--- backend/src/test/java/com/empire/RegionTest.java
+++ backend/src/test/java/com/empire/RegionTest.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class RegionTest {
     private static Region r;
