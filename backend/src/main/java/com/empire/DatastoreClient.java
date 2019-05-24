89a15b0f0f0d24f57a283c6dd7d1872653bdd96f
Alexander Pawlicki <alex__pawlicki@bose.com>
2019-05-24 14:36:43 -0400

    Added as much of order as possible

--- backend/src/main/java/com/empire/DatastoreClient.java
+++ backend/src/main/java/com/empire/DatastoreClient.java
@@ -5,16 +5,20 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Text;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import java.util.HashMap;
 
 public class DatastoreClient {
     private static final String playerType = "Player";
     private static final String nationType = "Nation";
+    private static final String orderType = "Order";
 
     private static final String jsonProp = "json";
     private static final String passhashProp = "passHash";
+    private static final String versionProp = "version";
 
     private static DatastoreClient instance = null;
 
@@ -39,7 +43,7 @@ public class DatastoreClient {
         return new Player(email, (String)e.getProperty(passhashProp));
     }
 
-    public Entity playerToEntity(Player player) {
+    private Entity playerToEntity(Player player) {
         Entity e = new Entity(playerType, player.email);
         e.setProperty(passhashProp, player.passHash);
         return e;
@@ -48,18 +52,48 @@ public class DatastoreClient {
     // Nation
 
     public Nation.NationGson loadNation(String nation, long gameId) throws EntityNotFoundException {
-        Entity e = service.get(KeyFactory.createKey(nationType, gameId + "_" + nation));
+        Entity e = service.get(KeyFactory.createKey(nationType, createNationkey(nation, gameId)));
         String jsonStr = (String) e.getProperty(jsonProp);
         return getGson().fromJson(jsonStr, Nation.NationGson.class);
     }
 
-    public Entity nationToEntity(String nation, long gameId) {
-        Entity e = new Entity(nationType, gameId + "_" + nation);
+    private Entity nationToEntity(String nation, long gameId) {
+        Entity e = new Entity(nationType, createNationkey(nation, gameId));
         e.setProperty(jsonProp, getGson().toJson(this));
         return e;
     }
 
+    private String createNationkey(String nation, long gameId) {
+        return gameId + "_" + nation;
+    }
+
     private Gson getGson() {
         return new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
+    }
+
+    // Order
+
+    public Order loadOrder(long gameId, String kingdom, int turn) throws EntityNotFoundException {
+        Entity e = service.get(KeyFactory.createKey(orderType, createOrderkey(gameId, kingdom, turn)));
+        return new Order(gameId, kingdom, turn, (int)((Long)e.getProperty("version")).longValue(), ((Text)e.getProperty("json")).getValue());
+    }
+
+//    public HashMap<String, String> getOrders() {
+//        return
+//                new GsonBuilder()
+//                        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+//                        .create()
+//                        .fromJson(json, Order.OrderGson.class).orders;
+//    }
+
+    private Entity orderToEntity(Order order) {
+        Entity e = new Entity(orderType, createOrderkey(order.gameId, order.kingdom, order.turn));
+        e.setProperty(jsonProp, new Text(order.json));
+        e.setProperty(versionProp, order.version);
+        return e;
+    }
+
+    private String createOrderkey(long gameId, String kingdom, int turn) {
+        return gameId + "_" + turn + "_" + kingdom;
     }
 }
