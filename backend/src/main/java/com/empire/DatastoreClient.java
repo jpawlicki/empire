a0dddcbf03efffc6c301d5746b66294afdc007dd
Alexander Pawlicki <alex__pawlicki@bose.com>
2019-05-24 14:00:13 -0400

    Player communication to Datastore

--- backend/src/main/java/com/empire/DatastoreClient.java
+++ backend/src/main/java/com/empire/DatastoreClient.java
@@ -2,8 +2,14 @@ package com.empire;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.KeyFactory;
 
 public class DatastoreClient {
+    private static final String playerType = "Player";
+    private static final String passhashProp = "passHash";
+
     private static DatastoreClient instance = null;
 
     private final DatastoreService service;
@@ -18,5 +24,16 @@ public class DatastoreClient {
 
     private DatastoreClient(DatastoreService service) {
         this.service = service;
+    }
+
+    public Player loadPlayer(String email) throws EntityNotFoundException {
+        Entity e = service.get(KeyFactory.createKey(playerType, email));
+        return new Player(email, (String)e.getProperty(passhashProp));
+    }
+
+    public Entity playerToEntity(Player player) {
+        Entity e = new Entity(playerType, player.email);
+        e.setProperty(passhashProp, player.passHash);
+        return e;
     }
 }
