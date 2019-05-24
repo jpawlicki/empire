a6acb34d7a102aac15f52b460d6d775872318766
Alexander Pawlicki <alex__pawlicki@bose.com>
2019-05-24 14:44:30 -0400

    Shadow class for Nation

---
+++ backend/src/main/java/com/empire/store/Nation.java
@@ -0,0 +1,29 @@
+package com.empire.store;
+
+//import com.empire.Ideology;
+
+public class Nation {
+    public static String TYPE = "Nation";
+    public String rulerName = "";
+    public String title = "";
+    public String food = "";
+    public String happiness = "";
+    public String territory = "";
+    public String glory = "";
+    public String religion = "";
+    public String ideology = "";
+    public String security = "";
+    public String riches = "";
+    public String culture = "";
+    public String trait1 = "";
+    public String trait2 = "";
+//    public Ideology dominantIdeology;
+    public String bonus = "";
+    public String email = "";
+    public String password = "";
+
+//    @Override
+//    public String toString() {
+//        return getGson().toJson(this);
+//    }
+}
