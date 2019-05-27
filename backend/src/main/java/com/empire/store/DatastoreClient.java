2d5d6e174097fe924a6c4b7ef84530df9d11eb52
Alexander Pawlicki <alex__pawlicki@bose.com>
2019-05-26 21:43:50 -0400

    Added methods to DatastoreClient interface

--- backend/src/main/java/com/empire/store/DatastoreClient.java
+++ backend/src/main/java/com/empire/store/DatastoreClient.java
@@ -1,5 +1,8 @@
 package com.empire.store;
 
 public interface DatastoreClient {
-
+     Player loadPlayer(String email);
+     Nation loadNation(String nation, long gameId);
+     Orders loadOrders(long gameId, String kingdom, int turn);
+     World loadWorld(long gameId, int turn);
 }
