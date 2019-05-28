f33f6deca37f75a5367bdc28480b19adcd7d5f97
Alexander Pawlicki <alex__pawlicki@bose.com>
2019-05-28 11:21:48 -0400

    Moved client tests to test package and used Datastore test stubs

---
+++ backend/src/test/java/com/empire/store/GaeDatastoreClientTest.java
@@ -0,0 +1,44 @@
+package com.empire.store;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GaeDatastoreClientTest {
+    private final LocalServiceTestHelper helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+
+    @Before
+    public void setUp(){
+        helper.setUp();
+    }
+
+    @After
+    public void tearDown(){
+        helper.tearDown();
+    }
+
+    @Test
+    public void basicTest(){
+        GaeDatastoreClient client = GaeDatastoreClient.getInstance();
+        long gameId = -1;
+        int turn = 4;
+        int version = 2;
+        String kingdom = "Test Kingdom";
+        Map<String, String> ordersMap = new HashMap<>();
+        ordersMap.put("Key0", "Order0");
+        ordersMap.put("Key1", "Order1");
+        ordersMap.put("Key2", "Order2");
+
+        Orders ordersRead0 = client.getOrders(gameId, kingdom, turn);
+
+        Orders orders = new Orders(gameId, turn, version, kingdom, ordersMap);
+        boolean resultPut = client.putOrders(orders);
+
+        Orders ordersRead = client.getOrders(gameId, kingdom, turn);
+        System.out.println("Done");
+    }
+}
