6b47726320f8a14e1a1f95140a9a349846ce0342
Alexander Pawlicki <alex__pawlicki@bose.com>
2019-05-28 18:04:31 -0400

    Line cleanup

--- backend/src/main/java/com/empire/store/Orders.java
+++ backend/src/main/java/com/empire/store/Orders.java
@@ -3,13 +3,11 @@ package com.empire.store;
 import java.util.Map;
 
 public class Orders {
-
     public final long gameId;
     public final int turn;
     public final int version;
     public final String kingdom;
     public final Map<String, String> orders;
-
 
     public Orders(long gameId, int turn, int version, String kingdom, Map<String, String> orders) {
         this.gameId = gameId;
