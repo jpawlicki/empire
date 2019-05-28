22b92ae9e191ee694aac692e5c09cc247d3210c5
Alexander Pawlicki <alex__pawlicki@bose.com>
2019-05-28 08:24:31 -0400

    Filled out DatastoreClient interface and GAE impl

--- backend/src/main/java/com/empire/store/GaeDatastoreClient.java
+++ backend/src/main/java/com/empire/store/GaeDatastoreClient.java
@@ -11,7 +11,10 @@ import com.google.appengine.api.datastore.TransactionOptions;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
 import java.util.Collections;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class GaeDatastoreClient implements DatastoreClient{
@@ -22,12 +25,16 @@ public class GaeDatastoreClient implements DatastoreClient{
     private static final String orderType = "Order";
     private static final String worldType = "World";
     private static final String activeType = "Active";
+    private static final String currentDateType = "CURRENTDATE";
+    private static final String activeGamesType = "ACTIVEGAMES";
 
+    private static final String dateProp = "date";
     private static final String jsonProp = "json";
     private static final String jsonGzipProp = "json_gzip";
     private static final String passhashProp = "passHash";
     private static final String versionProp = "version";
     private static final String loginProp = "login";
+    private static final String activeGamesProp = "active_games";
 
     private static GaeDatastoreClient instance = null;
     public static Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
@@ -76,6 +83,11 @@ public class GaeDatastoreClient implements DatastoreClient{
         }
     }
 
+    @Override
+    public boolean putPlayer(Player player) {
+        return putEntityInTransaction(playerToEntity(player));
+    }
+
     private Entity playerToEntity(Player player) {
         Entity e = new Entity(playerType, player.email);
         e.setProperty(passhashProp, player.passHash);
@@ -85,14 +97,22 @@ public class GaeDatastoreClient implements DatastoreClient{
     // Nation
 
     public Nation getNation(String nation, long gameId) {
+        return gson.fromJson(getNationJson(nation, gameId), Nation.class);
+    }
+
+    public String getNationJson(String nation, long gameId) {
         try {
             Entity e = service.get(KeyFactory.createKey(nationType, createNationkey(nation, gameId)));
-            String jsonStr = (String) e.getProperty(jsonProp);
-            return gson.fromJson(jsonStr, Nation.class);
+            return (String) e.getProperty(jsonProp);
         } catch (EntityNotFoundException e) {
             log.severe("Enable to retrieve Nation with key " + createNationkey(nation, gameId));
             return null;
         }
+    }
+
+    @Override
+    public boolean putNation(long gameId, String nation) {
+        return putEntityInTransaction(nationToEntity(nation, gameId));
     }
 
     private Entity nationToEntity(String nation, long gameId) {
@@ -116,6 +136,11 @@ public class GaeDatastoreClient implements DatastoreClient{
             log.severe("Enable to retrieve Orders with key " + createOrderkey(gameId, kingdom, turn));
             return null;
         }
+    }
+
+    @Override
+    public boolean putOrders(Orders orders) {
+        return putEntityInTransaction(orderToEntity(orders));
     }
 
     private Entity orderToEntity(Orders orders) {
@@ -150,6 +175,11 @@ public class GaeDatastoreClient implements DatastoreClient{
         }
     }
 
+    @Override
+    public boolean putWorld(long gameId, World world) {
+        return putEntityInTransaction(worldToEntity(gameId, world));
+    }
+
     private Entity worldToEntity(long gameId, World world) {
 		Entity e = new Entity(worldType, createWorldKey(gameId, world.date));
 		String jsonStr = gson.toJson(world);
@@ -162,11 +192,69 @@ public class GaeDatastoreClient implements DatastoreClient{
         return gameId + "_" + turn;
     }
 
+    // World Date
+
+    public int getWorldDate(long gameId) {
+        try {
+            Entity e = service.get(KeyFactory.createKey(currentDateType, createCurrentDateKey(gameId)));
+            Long date = (Long) e.getProperty(dateProp);
+            return (int) date.longValue();
+
+        } catch (EntityNotFoundException e){
+            log.severe("Enable to retrieve Current Date with key " + createCurrentDateKey(gameId));
+            return -1;
+        }
+    }
+
+    @Override
+    public boolean putWorldDate(long gameId, int date) {
+        return putEntityInTransaction(dateToEntity(gameId, date));
+    }
+
+    private Entity dateToEntity(long gameId, int date) {
+        Entity entity = new Entity(currentDateType, createCurrentDateKey(gameId));
+        entity.setProperty(dateProp, (long) date);
+        return entity;
+    }
+
+    private String createCurrentDateKey(long gameId){
+        return "game_" + gameId;
+    }
+
+    // Active games
+
+    public List<Long> getActiveGames() {
+        try {
+            Entity e = service.get(KeyFactory.createKey(activeGamesType, createActiveGamesKey()));
+            String jsonStr = (String) e.getProperty(activeGamesProp);
+            Type listType = new TypeToken<List<Long>>(){}.getType();
+            return gson.fromJson(jsonStr, listType);
+        } catch (EntityNotFoundException e) {
+            log.severe("Enable to retrieve active games with key " + createActiveGamesKey());
+            return null;
+        }
+    }
+
+    @Override
+    public boolean putActiveGames(List<Long> activeGames) {
+        return putEntityInTransaction(activeGamesToEntity(activeGames));
+    }
+
+    private Entity activeGamesToEntity(List<Long> activeGames){
+        Entity entity = new Entity(activeGamesType, createActiveGamesKey());
+        entity.setProperty(activeGamesProp, gson.toJson(activeGames));
+        return entity;
+    }
+
+    private String createActiveGamesKey(){
+        return "_";
+    }
+
     // Login
 
     public LoginKey getLogin(long gameId, int date, String email) {
         try {
-            Entity e = service.get(KeyFactory.createKey("Active", gameId + "_" + date + "_" + email));
+            Entity e = service.get(KeyFactory.createKey(activeType, createLoginKey(gameId, date, email)));
             return new LoginKey("", 0, 0);
         } catch (EntityNotFoundException e) {
             log.severe("Enable to retrieve Login with key " + createLoginKey(gameId, date, email));
