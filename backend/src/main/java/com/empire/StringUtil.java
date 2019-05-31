c374ccefc2444e362480214fcb429071439c06e4
Alexander Pawlicki <alex__pawlicki@bose.com>
2019-05-31 13:49:23 -0400

    Moved read resources function to StringUtils

--- backend/src/main/java/com/empire/StringUtil.java
+++ backend/src/main/java/com/empire/StringUtil.java
@@ -1,5 +1,8 @@
 package com.empire;
 
+import com.google.appengine.repackaged.com.google.common.io.Resources;
+import com.google.common.base.Charsets;
+import java.io.IOException;
 import java.util.List;
 
 public class StringUtil {
@@ -17,6 +20,14 @@ public class StringUtil {
 	public static String quantify(int num, String unit) {
 		if (unit.endsWith("s")) return num + " " + unit;
 		return num + " " + unit + (num != 1 ? "s" : "");
+	}
+
+	public static String readResourceAsString(String resource) {
+		try {
+			return Resources.toString(Resources.getResource(resource), Charsets.UTF_8);
+		} catch(IOException e){
+			return null;
+		}
 	}
 
 	private StringUtil() {}
