398f5b02cc8f9973cda5febe19c582013c4f96f6
Joshua Pawlicki <gilgarn@gmail.com>
2019-06-05 03:33:22 -0700

    spaces2tabs

--- backend/src/test/java/com/empire/CharacterTest.java
+++ backend/src/test/java/com/empire/CharacterTest.java
@@ -10,79 +10,79 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class CharacterTest {
-    private Character c;
-    private static World w;
-    private static NationData n1;
-    private static Region r1;
-    private static final double DELTA = 1E-5;
+	private Character c;
+	private static World w;
+	private static NationData n1;
+	private static Region r1;
+	private static final double DELTA = 1E-5;
 
-    private static final String k1 = "k1";
+	private static final String k1 = "k1";
 
-    @Before
-    public void createCharacter() {
-        c = new Character();
-        c.kingdom = k1;
+	@Before
+	public void createCharacter() {
+		c = new Character();
+		c.kingdom = k1;
 
-        w = mockWorld();
-    }
+		w = mockWorld();
+	}
 
-    private World mockWorld(){
-        World world = mock(World.class);
+	private World mockWorld(){
+		World world = mock(World.class);
 
-        n1 = mock(NationData.class);
-        when(world.getNation(k1)).thenReturn(n1);
+		n1 = mock(NationData.class);
+		when(world.getNation(k1)).thenReturn(n1);
 
-        r1 = Mocks.region(k1, Region.Type.LAND, 1.0, Ideology.SWORD_OF_TRUTH);
-        world.regions = Collections.singletonList(r1);
+		r1 = Mocks.region(k1, Region.Type.LAND, 1.0, Ideology.SWORD_OF_TRUTH);
+		world.regions = Collections.singletonList(r1);
 
-        return world;
-    }
+		return world;
+	}
 
-    @Test
-    public void calcPlotPowerBasic() {
-        assertEquals(1.3, c.calcPlotPower(w, false, 0), DELTA);
-    }
+	@Test
+	public void calcPlotPowerBasic() {
+		assertEquals(1.3, c.calcPlotPower(w, false, 0), DELTA);
+	}
 
-    @Test
-    public void calcPlotPowerSpyLevel() {
-        c.addExperienceSpy();
-        c.addExperienceSpy();
-        c.addExperienceSpy();
-        assertEquals(1.6, c.calcPlotPower(w, false, 0), DELTA);
-    }
+	@Test
+	public void calcPlotPowerSpyLevel() {
+		c.addExperienceSpy();
+		c.addExperienceSpy();
+		c.addExperienceSpy();
+		assertEquals(1.6, c.calcPlotPower(w, false, 0), DELTA);
+	}
 
-    @Test
-    public void calcPlotPowerGuarding() {
-        assertEquals(1.8, c.calcPlotPower(w, true, 0), DELTA);
-    }
+	@Test
+	public void calcPlotPowerGuarding() {
+		assertEquals(1.8, c.calcPlotPower(w, true, 0), DELTA);
+	}
 
-    @Test
-    public void calcPlotPowerLyskr() {
-        r1.religion = Ideology.LYSKR;
-        assertEquals(1.7, c.calcPlotPower(w, false, 0), DELTA);
-    }
+	@Test
+	public void calcPlotPowerLyskr() {
+		r1.religion = Ideology.LYSKR;
+		assertEquals(1.7, c.calcPlotPower(w, false, 0), DELTA);
+	}
 
-    @Test
-    public void calcPlotPowerCompany() {
-        r1.religion = Ideology.COMPANY;
-        assertEquals(1.5, c.calcPlotPower(w, false, 0), DELTA);
-    }
+	@Test
+	public void calcPlotPowerCompany() {
+		r1.religion = Ideology.COMPANY;
+		assertEquals(1.5, c.calcPlotPower(w, false, 0), DELTA);
+	}
 
-    @Test
-    public void calcPlotPowerInspireNonIruhan() {
-        r1.religion = Ideology.ALYRJA;
-        assertEquals(1.3, c.calcPlotPower(w, false, 2), DELTA);
-    }
+	@Test
+	public void calcPlotPowerInspireNonIruhan() {
+		r1.religion = Ideology.ALYRJA;
+		assertEquals(1.3, c.calcPlotPower(w, false, 2), DELTA);
+	}
 
-    @Test
-    public void calcPlotPowerInspireIruhan() {
-        r1.religion = Ideology.SWORD_OF_TRUTH;
-        assertEquals(1.4, c.calcPlotPower(w, false, 2), DELTA);
-    }
+	@Test
+	public void calcPlotPowerInspireIruhan() {
+		r1.religion = Ideology.SWORD_OF_TRUTH;
+		assertEquals(1.4, c.calcPlotPower(w, false, 2), DELTA);
+	}
 
-    @Test
-    public void calcPlotPowerCaptive() {
-        c.captor = "DONTCARE";
-        assertEquals(0.8, c.calcPlotPower(w, false, 0), DELTA);
-    }
+	@Test
+	public void calcPlotPowerCaptive() {
+		c.captor = "DONTCARE";
+		assertEquals(0.8, c.calcPlotPower(w, false, 0), DELTA);
+	}
 }
--- backend/src/test/java/com/empire/RegionTest.java
+++ backend/src/test/java/com/empire/RegionTest.java
@@ -13,305 +13,305 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class RegionTest {
-    private static Region r;
-    private static World w;
-    private static NationData n1;
-    private static final double DELTA = 1E-5;
+	private static Region r;
+	private static World w;
+	private static NationData n1;
+	private static final double DELTA = 1E-5;
 
-    private static final String k1 = "k1";
-    private static final String k2 = "k2";
-    private static final String k3 = "k3";
+	private static final String k1 = "k1";
+	private static final String k2 = "k2";
+	private static final String k3 = "k3";
 
-    private static final double unrestMiddle = 0.25;
-    private static final double unrestLower = 0.15;
-    private static final double unrestHigher = 0.35;
-    private static final double pop = 1E4;
+	private static final double unrestMiddle = 0.25;
+	private static final double unrestLower = 0.15;
+	private static final double unrestHigher = 0.35;
+	private static final double pop = 1E4;
 
-    @Before
-    public void setUpRegion() {
-        r = new Region();
-				r.type = Region.Type.LAND;
-        r.setKingdomNoScore(k1);
-        r.religion = Ideology.COMPANY;
-        r.population = pop;
-        r.unrestPopular = unrestMiddle;
-        r.setKingdomNoScore(k1);
+	@Before
+	public void setUpRegion() {
+		r = new Region();
+		r.type = Region.Type.LAND;
+		r.setKingdomNoScore(k1);
+		r.religion = Ideology.COMPANY;
+		r.population = pop;
+		r.unrestPopular = unrestMiddle;
+		r.setKingdomNoScore(k1);
 
-        w = mockWorld(r);
-    }
+		w = mockWorld(r);
+	}
 
-    private World mockWorld(Region r){
-        World world = mock(World.class);
-        Region r1 = mockRegion(k1, Region.Type.LAND, Ideology.CHALICE_OF_COMPASSION);
-        Region r2 = mockRegion(k1, Region.Type.LAND, Ideology.CHALICE_OF_COMPASSION);
-        Region r3 = mockRegion(k1, Region.Type.LAND, Ideology.SWORD_OF_TRUTH);
-        Region r4 = mockRegion(k2, Region.Type.LAND, null);
-        Region r5 = mockRegion(k3, Region.Type.WATER, null);
-        world.regions = Arrays.asList(r, r1, r2, r3, r4, r5);
+	private World mockWorld(Region r){
+		World world = mock(World.class);
+		Region r1 = mockRegion(k1, Region.Type.LAND, Ideology.CHALICE_OF_COMPASSION);
+		Region r2 = mockRegion(k1, Region.Type.LAND, Ideology.CHALICE_OF_COMPASSION);
+		Region r3 = mockRegion(k1, Region.Type.LAND, Ideology.SWORD_OF_TRUTH);
+		Region r4 = mockRegion(k2, Region.Type.LAND, null);
+		Region r5 = mockRegion(k3, Region.Type.WATER, null);
+		world.regions = Arrays.asList(r, r1, r2, r3, r4, r5);
 
-        Pirate p = mock(Pirate.class);
-        p.bribes = new HashMap<>();
-        p.bribes.put(k1, 0.0);
-        world.pirate = p;
+		Pirate p = mock(Pirate.class);
+		p.bribes = new HashMap<>();
+		p.bribes.put(k1, 0.0);
+		world.pirate = p;
 
-        n1 = mock(NationData.class);
-        when(world.getNation(k1)).thenReturn(n1);
+		n1 = mock(NationData.class);
+		when(world.getNation(k1)).thenReturn(n1);
 
-        return world;
-    }
+		return world;
+	}
 
-    private Region mockRegion(String kingdom, Region.Type type, Ideology religion){
-        Region r = mock(Region.class);
-        when(r.getKingdom()).thenReturn(kingdom);
-        r.type = type;
-        r.religion = religion;
-        return r;
-    }
+	private Region mockRegion(String kingdom, Region.Type type, Ideology religion){
+		Region r = mock(Region.class);
+		when(r.getKingdom()).thenReturn(kingdom);
+		r.type = type;
+		r.religion = religion;
+		return r;
+	}
 
-    @Test
-    public void numUniqueIdeologiesTest(){
-        assertEquals(3, Region.numUniqueIdeologies(k1, w));
-    }
+	@Test
+	public void numUniqueIdeologiesTest(){
+		assertEquals(3, Region.numUniqueIdeologies(k1, w));
+	}
 
-    // TODO(s):  The can transfer food tests rely on making a test w that is too complex at the moment, these should
-    // be revisited if/once refactoring has made this more feasible
+	// TODO(s):	The can transfer food tests rely on making a test w that is too complex at the moment, these should
+	// be revisited if/once refactoring has made this more feasible
 
-    @Test
-    public void canTransferFoodToAdjacent(){
-        // Test when r0 and r1 are directly adjacent one another, assert true
-        //assertTrue(r0.canFoodTransferTo(w, r1));
-        assertTrue(true);
-    }
+	@Test
+	public void canTransferFoodToAdjacent(){
+		// Test when r0 and r1 are directly adjacent one another, assert true
+		//assertTrue(r0.canFoodTransferTo(w, r1));
+		assertTrue(true);
+	}
 
-    @Test
-    public void canTransferFoodToViaOcean(){
-        // Test when r0 and r1 connect via ocean, assert true
-        //assertTrue(r0.canFoodTransferTo(w, r1));
-        assertTrue(true);
-    }
+	@Test
+	public void canTransferFoodToViaOcean(){
+		// Test when r0 and r1 connect via ocean, assert true
+		//assertTrue(r0.canFoodTransferTo(w, r1));
+		assertTrue(true);
+	}
 
-    @Test
-    public void canTransferFoodToFalse(){
-        // Test when r0 and r1 are neither adjacent nor connected via ocean, assert false
-        //assertFalse(r0.canFoodTransferTo(w, r1));
-        assertFalse(false);
-    }
+	@Test
+	public void canTransferFoodToFalse(){
+		// Test when r0 and r1 are neither adjacent nor connected via ocean, assert false
+		//assertFalse(r0.canFoodTransferTo(w, r1));
+		assertFalse(false);
+	}
 
-    @Test
-    public void canTransferFoodToSelf(){
-        // Test when r0 and r1 are the same region, assert false
-        //assertFalse(r0.canFoodTransferTo(w, r1));
-        assertFalse(false);
-    }
+	@Test
+	public void canTransferFoodToSelf(){
+		// Test when r0 and r1 are the same region, assert false
+		//assertFalse(r0.canFoodTransferTo(w, r1));
+		assertFalse(false);
+	}
 
-    @Test
-    public void calcUnrestClericalNonIruhan(){
-        r.religion = Ideology.ALYRJA;
-        assertEquals(0.0, r.calcUnrestClerical(unused -> -25), DELTA);
-    }
+	@Test
+	public void calcUnrestClericalNonIruhan(){
+		r.religion = Ideology.ALYRJA;
+		assertEquals(0.0, r.calcUnrestClerical(unused -> -25), DELTA);
+	}
 
-    @Test
-    public void calcUnrestVesselOfFaith(){
-        r.religion = Ideology.VESSEL_OF_FAITH;
-        assertEquals(0.0, r.calcUnrestClerical(unused -> -25), DELTA);
-    }
+	@Test
+	public void calcUnrestVesselOfFaith(){
+		r.religion = Ideology.VESSEL_OF_FAITH;
+		assertEquals(0.0, r.calcUnrestClerical(unused -> -25), DELTA);
+	}
 
-    @Test
-    public void calcUnrestClericalIruhan(){
-        r.religion = Ideology.SWORD_OF_TRUTH;
-        assertEquals(0.5, r.calcUnrestClerical(unused -> -50), DELTA);
-    }
+	@Test
+	public void calcUnrestClericalIruhan(){
+		r.religion = Ideology.SWORD_OF_TRUTH;
+		assertEquals(0.5, r.calcUnrestClerical(unused -> -50), DELTA);
+	}
 
-    @Test
-    public void calcUnrestNobleEmptyName(){
-        r.noble = Noble.makeNoble(Culture.ANPILAYN, 1);
-        r.noble.name = "";
-				r.noble.unrest = unrestMiddle;
-        assertEquals(0.0, r.calcUnrestNoble(), DELTA);
-    }
+	@Test
+	public void calcUnrestNobleEmptyName(){
+		r.noble = Noble.makeNoble(Culture.ANPILAYN, 1);
+		r.noble.name = "";
+		r.noble.unrest = unrestMiddle;
+		assertEquals(0.0, r.calcUnrestNoble(), DELTA);
+	}
 
-    @Test
-    public void calcUnrestNoblePresent(){
-        r.noble = Noble.makeNoble(Culture.ANPILAYN, 1);
-				r.noble.unrest = unrestMiddle;
-        assertEquals(unrestMiddle, r.calcUnrestNoble(), DELTA);
-    }
+	@Test
+	public void calcUnrestNoblePresent(){
+		r.noble = Noble.makeNoble(Culture.ANPILAYN, 1);
+		r.noble.unrest = unrestMiddle;
+		assertEquals(unrestMiddle, r.calcUnrestNoble(), DELTA);
+	}
 
-    @Test
-    public void calcUnrestAll(){
-        r.unrestPopular = unrestLower;
-        assertEquals(unrestLower, r.calcUnrest(unused -> -25), DELTA);
+	@Test
+	public void calcUnrestAll(){
+		r.unrestPopular = unrestLower;
+		assertEquals(unrestLower, r.calcUnrest(unused -> -25), DELTA);
 
-        r.religion = Ideology.SWORD_OF_TRUTH;
-        assertEquals(0.25, r.calcUnrest(unused -> -25), DELTA);
+		r.religion = Ideology.SWORD_OF_TRUTH;
+		assertEquals(0.25, r.calcUnrest(unused -> -25), DELTA);
 
-        r.noble = Noble.makeNoble(Culture.ANPILAYN, 1);
-				r.noble.unrest = unrestHigher;
-        assertEquals(unrestHigher, r.calcUnrest((unused -> -25)), DELTA);
-    }
+		r.noble = Noble.makeNoble(Culture.ANPILAYN, 1);
+		r.noble.unrest = unrestHigher;
+		assertEquals(unrestHigher, r.calcUnrest((unused -> -25)), DELTA);
+	}
 
-    @Test
-    public void calcBaseConquestStrength(){
-        r.noble = null;
-        assertEquals(5.25, r.calcBaseConquestStrength(w), DELTA);
-    }
+	@Test
+	public void calcBaseConquestStrength(){
+		r.noble = null;
+		assertEquals(5.25, r.calcBaseConquestStrength(w), DELTA);
+	}
 
-    @Test
-    public void calcMinConquestStrengthStoic(){
-        when(n1.hasTag(NationData.Tag.STOIC)).thenReturn(true);
-        assertEquals(9.1875, r.calcMinConquestStrength(w), DELTA);
-    }
+	@Test
+	public void calcMinConquestStrengthStoic(){
+		when(n1.hasTag(NationData.Tag.STOIC)).thenReturn(true);
+		assertEquals(9.1875, r.calcMinConquestStrength(w), DELTA);
+	}
 
-    @Test
-    public void calcMinConquestStrengthFortified(){
-        r.constructions = Arrays.asList(Construction.makeFortifications(0), Construction.makeFortifications(0));
-        assertEquals(6.825, r.calcMinConquestStrength(w), DELTA);
-    }
+	@Test
+	public void calcMinConquestStrengthFortified(){
+		r.constructions = Arrays.asList(Construction.makeFortifications(0), Construction.makeFortifications(0));
+		assertEquals(6.825, r.calcMinConquestStrength(w), DELTA);
+	}
 
-    @Test
-    public void calcMinPatrolStrength(){
-        r.unrestPopular = 0.4;
-        assertEquals(3.3, r.calcMinPatrolStrength(w), DELTA);
-    }
+	@Test
+	public void calcMinPatrolStrength(){
+		r.unrestPopular = 0.4;
+		assertEquals(3.3, r.calcMinPatrolStrength(w), DELTA);
+	}
 
-    @Test
-    public void calcFortificationMod(){
-        r.constructions = Arrays.asList(Construction.makeFortifications(0), Construction.makeFortifications(0));
-        assertEquals(0.3, r.calcFortificationMod(), DELTA);
-    }
+	@Test
+	public void calcFortificationMod(){
+		r.constructions = Arrays.asList(Construction.makeFortifications(0), Construction.makeFortifications(0));
+		assertEquals(0.3, r.calcFortificationMod(), DELTA);
+	}
 
-    @Test
-    public void calcSigningBonus(){
-        assertEquals(-1.0, r.calcSigningBonusMod(-2.0), DELTA);
-        assertEquals(-0.5, r.calcSigningBonusMod(-1.0), DELTA);
-        assertEquals(0.0, r.calcSigningBonusMod(0.0), DELTA);
-        assertEquals(1.0, r.calcSigningBonusMod(2.0), DELTA);
-        assertEquals(1.5, r.calcSigningBonusMod(4.0), DELTA);
-        assertEquals(2.0, r.calcSigningBonusMod(8.0), DELTA);
-    }
+	@Test
+	public void calcSigningBonus(){
+		assertEquals(-1.0, r.calcSigningBonusMod(-2.0), DELTA);
+		assertEquals(-0.5, r.calcSigningBonusMod(-1.0), DELTA);
+		assertEquals(0.0, r.calcSigningBonusMod(0.0), DELTA);
+		assertEquals(1.0, r.calcSigningBonusMod(2.0), DELTA);
+		assertEquals(1.5, r.calcSigningBonusMod(4.0), DELTA);
+		assertEquals(2.0, r.calcSigningBonusMod(8.0), DELTA);
+	}
 
-    @Test
-    public void calcConsumptionBasic(){
-        assertEquals(pop, r.calcConsumption(w, 1.0), DELTA);
-    }
+	@Test
+	public void calcConsumptionBasic(){
+		assertEquals(pop, r.calcConsumption(w, 1.0), DELTA);
+	}
 
-    @Test
-    public void calcConsumptionFoodMod(){
-        assertEquals(2E4, r.calcConsumption(w, 2.0), DELTA);
-    }
+	@Test
+	public void calcConsumptionFoodMod(){
+		assertEquals(2E4, r.calcConsumption(w, 2.0), DELTA);
+	}
 
-    @Test
-    public void calcConsumptionChalice(){
-        r.religion = Ideology.CHALICE_OF_COMPASSION;
-        assertEquals(8.5E3, r.calcConsumption(w, 1.0), DELTA);
-    }
+	@Test
+	public void calcConsumptionChalice(){
+		r.religion = Ideology.CHALICE_OF_COMPASSION;
+		assertEquals(8.5E3, r.calcConsumption(w, 1.0), DELTA);
+	}
 
-    @Test
-    public void calcPirateThreatBasic(){
-        r.unrestPopular = 0.0;
-        assertEquals(0.0, r.calcPirateThreat(w), DELTA);
-    }
+	@Test
+	public void calcPirateThreatBasic(){
+		r.unrestPopular = 0.0;
+		assertEquals(0.0, r.calcPirateThreat(w), DELTA);
+	}
 
-    @Test
-    public void calcPirateThreatUnrest(){
-        assertEquals(0.25, r.calcPirateThreat(w), DELTA);
-    }
+	@Test
+	public void calcPirateThreatUnrest(){
+		assertEquals(0.25, r.calcPirateThreat(w), DELTA);
+	}
 
-    @Test
-    public void calcPirateThreatWaterZero(){
-        r.type = Region.Type.WATER;
-        assertEquals(0.0, r.calcPirateThreat(w), DELTA);
-    }
+	@Test
+	public void calcPirateThreatWaterZero(){
+		r.type = Region.Type.WATER;
+		assertEquals(0.0, r.calcPirateThreat(w), DELTA);
+	}
 
-    @Test
-    public void calcPirateThreatAlyrjaZero(){
-        r.religion = Ideology.ALYRJA;
-        assertEquals(0.0, r.calcPirateThreat(w), DELTA);
-    }
+	@Test
+	public void calcPirateThreatAlyrjaZero(){
+		r.religion = Ideology.ALYRJA;
+		assertEquals(0.0, r.calcPirateThreat(w), DELTA);
+	}
 
-    @Test
-    public void calcPirateThreatNoble(){
-        r.noble = Noble.makeNoble(Culture.ANPILAYN, 1);
-        assertEquals(0.125, r.calcPirateThreat(w), DELTA);
-    }
+	@Test
+	public void calcPirateThreatNoble(){
+		r.noble = Noble.makeNoble(Culture.ANPILAYN, 1);
+		assertEquals(0.125, r.calcPirateThreat(w), DELTA);
+	}
 
 
-    @Test
-    public void calcPirateThreatBribe(){
-        w.pirate.bribes.put(k1, -90.0);
-        assertEquals(0.03125, r.calcPirateThreat(w), DELTA);
+	@Test
+	public void calcPirateThreatBribe(){
+		w.pirate.bribes.put(k1, -90.0);
+		assertEquals(0.03125, r.calcPirateThreat(w), DELTA);
 
-        w.pirate.bribes.put(k1, -60.0);
-        assertEquals(0.0625, r.calcPirateThreat(w), DELTA);
+		w.pirate.bribes.put(k1, -60.0);
+		assertEquals(0.0625, r.calcPirateThreat(w), DELTA);
 
-        w.pirate.bribes.put(k1, -30.0);
-        assertEquals(0.125, r.calcPirateThreat(w), DELTA);
+		w.pirate.bribes.put(k1, -30.0);
+		assertEquals(0.125, r.calcPirateThreat(w), DELTA);
 
-        w.pirate.bribes.put(k1, 30.0);
-        assertEquals(0.5, r.calcPirateThreat(w), DELTA);
+		w.pirate.bribes.put(k1, 30.0);
+		assertEquals(0.5, r.calcPirateThreat(w), DELTA);
 
-        w.pirate.bribes.put(k1, 60.0);
-        assertEquals(1.0, r.calcPirateThreat(w), DELTA);
+		w.pirate.bribes.put(k1, 60.0);
+		assertEquals(1.0, r.calcPirateThreat(w), DELTA);
 
-        w.pirate.bribes.put(k1, 90.0);
-        assertEquals(2.0, r.calcPirateThreat(w), DELTA);
-    }
+		w.pirate.bribes.put(k1, 90.0);
+		assertEquals(2.0, r.calcPirateThreat(w), DELTA);
+	}
 
-		@Test
-		public void plantChaliceOfCompassion() {
-			r.religion = Ideology.CHALICE_OF_COMPASSION;
-			r.population = 10000;
-			r.crops = 0;
-			r.plant(false);
-			assertEquals(2000, r.crops, DELTA);
-		}
+	@Test
+	public void plantChaliceOfCompassion() {
+		r.religion = Ideology.CHALICE_OF_COMPASSION;
+		r.population = 10000;
+		r.crops = 0;
+		r.plant(false);
+		assertEquals(2000, r.crops, DELTA);
+	}
 
-		@Test
-		public void plantAlyrja() {
-			r.religion = Ideology.ALYRJA;
-			r.population = 10000;
-			r.crops = 0;
-			r.plant(false);
-			assertEquals(0, r.crops, DELTA);
-		}
+	@Test
+	public void plantAlyrja() {
+		r.religion = Ideology.ALYRJA;
+		r.population = 10000;
+		r.crops = 0;
+		r.plant(false);
+		assertEquals(0, r.crops, DELTA);
+	}
 
-		@Test
-		public void plantHarvestTurn() {
-			r.population = 10000;
-			r.crops = 0;
-			r.plant(true);
-			assertEquals(130000, r.crops, DELTA);
-		}
+	@Test
+	public void plantHarvestTurn() {
+		r.population = 10000;
+		r.crops = 0;
+		r.plant(true);
+		assertEquals(130000, r.crops, DELTA);
+	}
 
-		@Test
-		public void harvest() {
-			r.population = 10000;
-			r.crops = 1000000;
-			r.food = 0;
-			r.harvest(new HashSet<>(), unused -> 0);
-			assertEquals(250000, r.food, DELTA);
-			assertEquals(0, r.crops, DELTA);
-		}
+	@Test
+	public void harvest() {
+		r.population = 10000;
+		r.crops = 1000000;
+		r.food = 0;
+		r.harvest(new HashSet<>(), unused -> 0);
+		assertEquals(250000, r.food, DELTA);
+		assertEquals(0, r.crops, DELTA);
+	}
 
-		@Test
-		public void harvestUnrest() {
-			r.population = 10000;
-			r.crops = 1000000;
-			r.food = 0;
-			r.unrestPopular = .95;
-			r.harvest(new HashSet<>(), unused -> 0);
-			assertEquals(75000, r.food, DELTA);
-			assertEquals(0, r.crops, DELTA);
-		}
+	@Test
+	public void harvestUnrest() {
+		r.population = 10000;
+		r.crops = 1000000;
+		r.food = 0;
+		r.unrestPopular = .95;
+		r.harvest(new HashSet<>(), unused -> 0);
+		assertEquals(75000, r.food, DELTA);
+		assertEquals(0, r.crops, DELTA);
+	}
 
-		@Test
-		public void harvestUnrestStoic() {
-			r.population = 10000;
-			r.crops = 1000000;
-			r.food = 0;
-			r.unrestPopular = .95;
-			r.harvest(Collections.singleton("k1"), unused -> 0);
-			assertEquals(250000, r.food, DELTA);
-			assertEquals(0, r.crops, DELTA);
-		}
+	@Test
+	public void harvestUnrestStoic() {
+		r.population = 10000;
+		r.crops = 1000000;
+		r.food = 0;
+		r.unrestPopular = .95;
+		r.harvest(Collections.singleton("k1"), unused -> 0);
+		assertEquals(250000, r.food, DELTA);
+		assertEquals(0, r.crops, DELTA);
+	}
 }
