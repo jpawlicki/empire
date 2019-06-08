87e95e26f53d314963916510bc18da4df02ff84f
Alexander Pawlicki <alex__pawlicki@bose.com>
2019-06-07 22:37:08 -0400

    No date returned test

--- backend/src/test/java/com/empire/svc/PasswordValidatorTest.java
+++ backend/src/test/java/com/empire/svc/PasswordValidatorTest.java
@@ -1,6 +1,7 @@
 package com.empire.svc;
 
 import com.empire.store.DatastoreClient;
+import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
@@ -13,18 +14,19 @@ public class PasswordValidatorTest {
   private static Request req;
 
 //  private final String emailTest = "test@email.com";
-//  private final long gameIdTest = -1;
+  private final long gameIdTest = -1;
 //  private final int turnTest = 5;
 //  private final String kingdomTest = "TEST_KINGDOM";
-//  private final String pwTest = "0123456789ABCDEF";
+  private final String pwTest = "0123456789ABCDEF";
 //  private final String pwEncTest = "CB469D8CFBD5CB38935AD4C8CBAE397A41A42ADCB85D2D5858550465F7BD31FC";
 
   @Before
   public void setup() {
-    DatastoreClient dsClient = mock(DatastoreClient.class);
+    dsClient = mock(DatastoreClient.class);
     passVal = new PasswordValidator(dsClient);
 
     req = mock(Request.class);
+    when(req.getGameId()).thenReturn(gameIdTest);
   }
 
   @Test
@@ -32,6 +34,14 @@ public class PasswordValidatorTest {
     when(req.getPassword()).thenReturn(null);
 
     assertEquals(passVal.checkPassword(req), PasswordValidator.PasswordCheck.FAIL);
+  }
+
+  @Test
+  public void noDateFoundReturnsNoEntity() {
+    when(req.getPassword()).thenReturn(pwTest);
+    when(dsClient.getWorldDate(gameIdTest)).thenReturn(Optional.empty());
+
+    assertEquals(passVal.checkPassword(req), PasswordValidator.PasswordCheck.NO_ENTITY);
   }
 
 //  private void passPasswordCheck() {
